26fe82641da029afa9ee8ac7ce569bc1ead7b6a9
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-01-15 19:11:31 +0300

    basic graphql implementation

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const {buildSchema} = require('graphql')
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use('/grapghql', graphqlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+        events : [String!]!
+
+    }
+
+    type RootMutation {
+        createEvent(name: String) : String
+    }
+        schema {
+            query: RootQuery
+            mutation:  RootMutation
+        }
+    `),
+    rootValue: {
+        events: ()=> { 
+            return ['Eat', 'Code', 'Repeat']; 
+        }
+        ,
+        createEvent: (eventInfos) => {
+            const eventName = eventInfos.name
+            return eventName;
+        }
+    },
+    graphiql : true
+    })
+);
 app.listen(3000)

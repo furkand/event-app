512d033ad76e6e46a16b0a82473909908f1ac0a2
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-01-21 21:42:35 +0300

    header acces controllers are changed

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ const app = express()
 const PORT = process.env.PORT  || 5000
 
 app.use(bodyParser.json())
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
+    res.setHeader("Access-Control-Allow-OrHeadersigin", "Content-Type,Authorization")
+    if(req.method === "OPTIONS"){
+        return res.sendStatus(200);
+    }
+    next()
+})
 app.use(isAuth)
-app.use('/grapghql', graphqlHttp({
+app.use('/graphql', graphqlHttp({
     schema:schema ,
     rootValue: resolver,
     graphiql : true
